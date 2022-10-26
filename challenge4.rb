be1e7ce569aa377224df7e849374a150574e4038
Caleb Thomas <cjthomas00@comcast.net>
2022-10-25 21:16:47 -0600

    Complete downcase challenge.

---
+++ challenge4.rb
@@ -0,0 +1,12 @@
+#Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+
+
+words = ["PeppeR", "ShOw", "COat", "SeLECtion"]
+
+words.each do |word|
+    puts word.downcase
+end
+
+
+# I need to have an array with both upper and lower case letters, then run a code to print every word in lowercase letters. I need to run a .each method, and then the .downcase command on each word. After that I will have to print it to the terminal with a puts command.
